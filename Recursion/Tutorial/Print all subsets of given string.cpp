8f479dd4f8d19821797c9f22276276419488b49f
Anmol Jaiswal <47267722+anmoljaiswal076@users.noreply.github.com>
2020-05-31 12:27:01 +0530

    Create Print all subsets of given string.cpp

---
+++ Recursion/Tutorial/Print all subsets of given string.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printSub(string str, string curr="",int index=0)
+{
+	if(index == str.length())
+	{
+		cout<<curr<<" ";
+		return;
+	}
+	printSub(str, curr, index+1);
+	printSub(str, curr+str[index],index+1);
+}
+
+int main()
+{
+	printSub("ABC");
+}
