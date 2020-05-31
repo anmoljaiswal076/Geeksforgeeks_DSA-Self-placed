ebd968803ac9dab560e7a311d8ee2e932dea6c73
Anmol Jaiswal <47267722+anmoljaiswal076@users.noreply.github.com>
2020-05-31 12:14:00 +0530

    Create Fibonacci Using Recursion.cpp

---
+++ Recursion/Problem/Fibonacci Using Recursion.cpp
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+//User function Template for C++
+
+//Complete this function
+long long fibonacci(int n)
+{
+    //Your code here
+    if(n==0)
+        return 1;
+    else if(n==1)
+        return 1;
+    else
+        return fibonacci(n-1)+fibonacci(n-2);
+}
+
+
+// { Driver Code Starts.
+
+
+
+int main() {
+	int T;
+	cin>>T;
+	while (T--)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    cout<<fibonacci(n)<<endl;
+	    
+	    
+	}
+	return 0;
+}  // } Driver Code Ends
